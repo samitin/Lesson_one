c58799b5c636e33874949481f83e182f798374c4
samitin <samitin.e@gmail.com>
2021-05-23 20:32:49 +1100

    код Activity главного экрана меню приложения

---
+++ app/src/main/java/ru/samitin/lesson_one/Task3Activity.java
@@ -0,0 +1,19 @@
+package ru.samitin.lesson_one;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Task3Activity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task3);
+
+
+    }
+}
