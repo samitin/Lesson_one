965a1f137b919625609c3b5ab6a864307ea15a3f
samitin <samitin.e@gmail.com>
2021-05-24 00:10:08 +1100

    Задание 2 код

--- app/src/main/java/ru/samitin/lesson_one/Task2Activity.java
+++ app/src/main/java/ru/samitin/lesson_one/Task2Activity.java
@@ -2,17 +2,50 @@ package ru.samitin.lesson_one;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Task2Activity extends AppCompatActivity {
 
-
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task2);
 
+        Button button = findViewById(R.id.task2_button);
+        EditText nik = findViewById(R.id.nik);
+        EditText name = findViewById(R.id.name);
+        EditText phone = findViewById(R.id.phone);
+        EditText age = findViewById(R.id.age);
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(),
+                        getTextEditText(nik, name, phone, age, email, password),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private String getTextEditText(EditText... editTexts) {
+        String text = "";
+        for (EditText et : editTexts) {
+            if (et == null || et.getText().toString().equals(""))
+                return "Поле " + et.getHint().toString() + " не заполненно!";
+            text += et.getHint().toString() + ": " + et.getText().toString() + "\n";
+        }
+        return text;
     }
 }
